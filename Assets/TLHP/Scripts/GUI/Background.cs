73f7f2fa9852ce3e7046098cdc535ba2df08c39a
Neville <103945243+nevillekyaw001@users.noreply.github.com>
2023-04-07 14:23:53 +0700

    Repo Name Changed
    JF 2 TL

---
+++ Assets/TLHP/Scripts/GUI/Background.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Background : MonoBehaviour
+{
+    public Sprite[] pictures;
+
+    // Use this for initialization
+    void OnEnable()
+    {
+        if (LevelManager.THIS != null)
+        {
+            var num = (int)((float)LevelManager.Instance.currentLevel / 20f - 0.01f);
+            GetComponent<Image>().sprite = pictures[num >= 5 ? 0 : num];
+            //GetComponent<Image>().sprite = pictures[(int)((float)LevelManager.Instance.currentLevel / 20f - 0.01f)];
+        }
+    }
+}
